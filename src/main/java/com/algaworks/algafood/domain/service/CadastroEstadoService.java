344189cbfc6779099297586cbb3e17999d7f6795
matheus5ilva <matheusxandre4@gmail.com>
2021-10-15 22:32:09 -0300

    projeto do curso especialista spring rest

---
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroEstadoService.java
@@ -0,0 +1,34 @@
+package com.algaworks.algafood.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.model.Estado;
+import com.algaworks.algafood.domain.repository.EstadoRepository;
+
+@Service
+public class CadastroEstadoService {
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	public Estado salvar(Estado estado) {
+		return estadoRepository.save(estado);
+	}
+
+	public void excluir(Long estadoId) {
+		try {
+			estadoRepository.deleteById(estadoId);
+		} catch (EmptyResultDataAccessException e) {
+			throw new EntidadeNaoEncontradaException(
+					String.format("Não existe um cadastro de estado com codigo %d", estadoId));
+		} catch (DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException(
+					String.format("Estado de codigo %d não pode ser removida, pois esta em uso", estadoId));
+		}
+	}
+}
---
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -0,0 +1,46 @@
+package com.algaworks.algafood.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+
+@Service
+public class CadastroRestauranteService {
+
+	@Autowired
+	private RestauranteRepository restauranteRepository;
+
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+
+	public Restaurante salvar(Restaurante restaurante) {
+
+		Long cozinhaId = restaurante.getCozinha().getId();
+		Cozinha cozinha = cozinhaRepository.findById(cozinhaId).orElseThrow(
+				() -> new EntidadeNaoEncontradaException(String.format("Não existe cozinha com codigo %d", cozinhaId)));
+
+		restaurante.setCozinha(cozinha);
+
+		return restauranteRepository.save(restaurante);
+	}
+
+	public void excluir(Long restauranteId) {
+		try {
+			restauranteRepository.deleteById(restauranteId);
+		} catch (EmptyResultDataAccessException e) {
+			throw new EntidadeNaoEncontradaException(
+					String.format("Não existe um cadastro de restaurante com codigo %d", restauranteId));
+		} catch (DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException(
+					String.format("Restaurante de codigo %d não pode ser removida, pois esta em uso", restauranteId));
+		}
+	}
+}
